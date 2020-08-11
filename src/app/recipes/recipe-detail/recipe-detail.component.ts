5254cec87005851bfcfb250afd839ed904b7017a
Ericcentrik <enaiman1@gmail.com>
2020-08-11 19:01:47 -0400

    finished creating forms for the recipes and shopping list components

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -33,4 +33,9 @@ id: number;
     this.router.navigate(['edit'], {relativeTo: this.route })
     // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route})
   }
+
+  onDeleteRecipe(){
+    this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(['/recipes'])
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,43 +1,58 @@
 import { Recipe } from './recipe.model';
-import {Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { Subject } from "rxjs"
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
-export class RecipeService{
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
   private recipes: Recipe[] = [
-        new Recipe(
-          'tasty Schnitzel', 
-          'A super-tasty Schnitzel - just awesome',
-          'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
-          [
-            new Ingredient('Meat', 1),
-            new Ingredient('French Fries', 20)
-          ]),
+    new Recipe(
+      'tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
 
-        new Recipe(
-          'Sous Vide pork loin', 
-          'Super Tender',
-          'https://images.pexels.com/photos/361184/asparagus-steak-veal-steak-veal-361184.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
-          [
-            new Ingredient('Pork loin', 1),
-            new Ingredient('Asparagus', 5),
-            new Ingredient('Tomatoe', 3),
-          ])
-      ];
+    new Recipe(
+      'Sous Vide pork loin',
+      'Super Tender',
+      'https://images.pexels.com/photos/361184/asparagus-steak-veal-steak-veal-361184.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      [
+        new Ingredient('Pork loin', 1),
+        new Ingredient('Asparagus', 5),
+        new Ingredient('Tomatoe', 3),
+      ])
+  ];
 
-      constructor(private slService: ShoppingListService){}
+  constructor(private slService: ShoppingListService) { }
 
-    getRecipes(){
-        return this.recipes.slice();
-    }
+  getRecipes() {
+    return this.recipes.slice();
+  }
 
-    getRecipe(index: number){
-      return this.recipes[index];
-    }
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
 
-    addIngredientsToShoppingList(ingredients: Ingredient[]){
-      this.slService.addIngredients(ingredients)
-    }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients)
+  }
+
+  addRecipces(recipe: Recipe) {
+    this.recipes.push(recipe)
+    this.recipesChanged.next(this.recipes.slice())
+  }
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice())
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice())
+  }
 }
