dd220b79510ded13a1faced825234fe4e5dca6bf
Ericcentrik <enaiman1@gmail.com>
2020-08-15 06:51:34 -0400

    created recipes and shopplist module to help make app module leaner

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
+import { CommonModule } from '@angular/common';
+
+import { ShoppingListComponent } from './shopping-list.component';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { SharedModule } from "../shared/shared.module";
+
+@NgModule({
+    declarations:[
+        ShoppingListComponent,
+        ShoppingEditComponent,
+    ],
+    imports: [
+        RouterModule.forChild([
+            { path: 'shopping-list', component: ShoppingListComponent },
+        ]),
+        FormsModule,
+        CommonModule,
+        SharedModule
+       
+    ]
+})
+export class ShoppingListModule{}
